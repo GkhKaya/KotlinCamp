f67e71509fc069117dee90fd774b19451f6370a5
gkhkaya <gkhkaya00@gmail.com>
2022-11-18 16:42:54 +0300

    Lesson of Create New Page added

---
+++ CreateNewPage/app/src/main/java/com/example/createnewpage/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.createnewpage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+/*    private lateinit var  myTextView : TextView
+    private lateinit var myButton : Button*/
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        /*myTextView = findViewById(R.id.myTextView)
+        myButton = findViewById(R.id.myButton)*/
+
+       /* myButton.setOnClickListener{
+            myTextView.text = "hello"
+        }*/
+
+        
+
+
+
+
+
+    }
+}
