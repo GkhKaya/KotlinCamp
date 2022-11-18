d86f0aaef9ffa9be492a549c97336e065d5fc4e6
gkhkaya <gkhkaya00@gmail.com>
2022-11-18 17:24:57 +0300

    Data Binding Connect to Design Properties

--- UseMVVM/app/src/main/java/com/example/usemvvm/MainActivity.kt
+++ UseMVVM/app/src/main/java/com/example/usemvvm/MainActivity.kt
@@ -2,37 +2,40 @@ package com.example.usemvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.usemvvm.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var design :ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        design = DataBindingUtil.setContentView(this,R.layout.activity_main)
 
-        result.text = "0"
+        design.result.text = "0"
 
-        sumButton.setOnClickListener{
-            val gotNumberOne =  numberOne.text.toString()
-            val gotNumberTwo =  numberTwo.text.toString()
+        design.sumButton.setOnClickListener{
+            val gotNumberOne = design.numberOne.text.toString()
+            val gotNumberTwo =  design.numberTwo.text.toString()
 
             val numberOne = gotNumberOne.toInt()
             val numberTwo= gotNumberTwo.toInt()
 
             val sum = numberOne+numberTwo
 
-            result.text = sum.toString()
+            design.result.text = sum.toString()
 
         }
-        multipliButton.setOnClickListener{
-            val gotNumberOne =  numberOne.text.toString()
-            val gotNumberTwo =  numberTwo.text.toString()
+        design.multipliButton.setOnClickListener{
+            val gotNumberOne =  design.numberOne.text.toString()
+            val gotNumberTwo =  design.numberTwo.text.toString()
 
             val numberOne = gotNumberOne.toInt()
             val numberTwo= gotNumberTwo.toInt()
 
             val multipli = numberOne*numberTwo
 
-            result.text = multipli.toString()
+            design.result.text =multipli.toString()
         }
     }
 
